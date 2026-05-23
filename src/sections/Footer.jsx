7f11485d7249c508e93f923c37fc00ded8e61057
Arya Vats <aryavats123@gmail.com>
2026-05-24 03:30:44 +0530

    sizing changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,9 +5,8 @@ export default function Footer() {
     <footer>
       <div className="footer-logo">Al<span>phants</span> Network</div>
       <ul className="footer-links">
-        <li><a href="#">LinkedIn</a></li>
-        <li><a href="#">Instagram</a></li>
-        <li><a href="#">Privacy</a></li>
+        <li><a href="https://www.linkedin.com/company/alphants-network/about/">LinkedIn</a></li>
+        <li><a href="https://www.instagram.com/alphantsnetwork?igsh=MWFscnJtczV3M2R1dA==">Instagram</a></li>
       </ul>
     </footer>
   );
